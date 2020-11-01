6a351560cd5723e4073f13d7030e815549743cdb
zi10ge <zi10ge@gmail.com>
2020-11-01 13:50:46 +0300

    fix

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
-output "public_ip_of_all_build_instances" {
+output "public_ip_of_build_instance" {
   description = "List of public IP addresses, if applicable"
   value       = ["${aws_instance.build_instance.*.public_ip}"]
 }
 
-output "public_ip_of_all_stage_instances" {
+output "public_ip_of_stage_instance" {
   description = "List of public IP addresses, if applicable"
   value       = ["${aws_instance.stage_instance.*.public_ip}"]
 }
